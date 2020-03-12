0bc8e6fdbcdbc1a038a91cdba28b55823e11d922
czhhhhh <2933880537@qq.com>
2020-03-12 21:21:27 +0800

    generic practice

---
+++ CSharpHomework/Homework4/Alarm/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alarm
+{
+    class Program
+    {
+        public delegate void AlarmHandler(int hour);
+        public delegate void TickHandler(int second, int minite, int hour);
+        public class CLock
+        {
+            private int minite;
+            private int hour;
+            private int second;
+            public int Minite { get; }
+            public int Second { get; }
+            public int Hour { get; }
+            public CLock(int hour,int minite,int second)
+            {
+                this.minite = minite;
+                this.second = second;
+                this.hour = hour;
+            }
+            public event AlarmHandler AlarmClock;
+            public event TickHandler TickClock;
+            public void WorkingClock()
+            {
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(1000);//waiting for one second
+                    if (second == 59)
+                    {
+                        second = 0;
+                        if (minite == 59)
+                        {
+                            minite = 0;
+                            if (hour == 23) { hour = 0; AlarmClock(hour); } else hour++;
+                        }
+                        else minite++;
+                    }
+                    else second++;
+                    TickClock(second, minite, hour);
+
+                }
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            CLock MyClock = new CLock(23, 59, 56);
+            void AlarmFunc(int hour)
+            {
+                Console.WriteLine($"This is {hour} o'clock now.");
+            }
+            void TickFunc(int second,int minite,int hour)
+            {
+                Console.WriteLine("Tic tok.");
+            }
+            MyClock.AlarmClock += AlarmFunc;
+            MyClock.TickClock += TickFunc;
+            MyClock.WorkingClock();
+        }
+    }
+}
---
+++ CSharpHomework/Homework4/GenericLink/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericLink
+{
+    class Node<T>
+    {
+        private Node<T> next;
+        private T nodeValue;
+        public Node<T> Next
+        {
+            get => next; set => next = value;
+        }
+        public T Value
+        {
+            get => nodeValue; set => nodeValue = value;
+        }
+        public Node(T t)
+        {
+            next = null;
+            nodeValue = t;
+        }
+    }
+
+    class GenericLink<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public GenericLink()
+        {
+            head = null;
+            tail = null;
+        }
+        public void AddToTail(T t)
+        {
+            Node<T> elem = new Node<T>(t);
+            if (tail == null)
+            {
+                head = elem;
+                tail = elem;
+            }
+            else
+            {
+                tail.Next = elem;
+                tail = elem;
+            }  
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node = head;
+            while (node != null)
+            {
+                action(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericLink<int> genericLink = new GenericLink<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                genericLink.AddToTail(i);
+            }
+            genericLink.ForEach(delegate (int x)
+            {
+                Console.WriteLine($"node value is {x}");
+            });
+            int sum = 0;
+            genericLink.ForEach(x => sum += x);
+            Console.WriteLine("the sum is {0}", sum);
+        }
+    }
+}
