f9a373a736d67f0115c83ae1e4f1cce02dc7ff5a
czhhhhh <2933880537@qq.com>
2020-03-22 12:49:06 +0800

    货物管理

---
+++ CSharpHomework/Homework5/OrderWarehouse/OrderService.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderWarehouse
+{
+    delegate bool Func(Order order);
+    class InvalidOperation: Exception
+    {
+        public string message { get; }
+        public InvalidOperation(string message) { this.message = message; }
+
+    }
+    class OrderService
+    {
+        List<Order> orders = new List<Order>();
+        public void CreateOrder(int id,string client)
+        {
+            try
+            {
+                int flag = 0;
+                foreach (Order i in orders) { if (id == i.OrderID) flag = 1; }//检测是否有重复订单号
+                if (flag == 0)
+                {
+                    Order NewOrder = new Order(id, client);
+                    //添加到订单序列
+                    orders.Add(NewOrder);
+                }
+                else
+                {
+                    throw new InvalidOperation("重复的订单号");
+                }
+            }
+            catch (InvalidOperation E)
+            {
+                Console.WriteLine("订单创建异常：" + E.message);
+            }
+            
+        }
+        public void DeleteOrder(int id)
+        {
+            try
+            {
+                int flag = 0;
+                for (int i = orders.Count() - 1; i >= 0; i--)
+                {
+                    if (id == orders[i].OrderID)
+                    {
+                        orders.Remove(orders[i]);
+                        flag = 1;
+                    }
+                }
+                if (flag == 1)
+                    Console.WriteLine("订单删除成功！");
+                else
+                {
+                    throw new InvalidOperation("不存在该订单！");
+                }
+            }catch(InvalidOperation e)
+            {
+                Console.WriteLine("订单删除异常：" + e.message);
+            }
+        }
+        public void AddOrderItem(int id ,string item ,int quantity ,double price)
+        {
+            try
+            {
+                int flag = 0;
+                
+                for(int i = orders.Count() - 1; i >= 0; i--)
+                {
+                    if (id == orders[i].OrderID)
+                    {
+                        flag = 1;
+                        orders[i].AddItem(item, quantity, price);
+                        Console.WriteLine("成功添加商品！");
+                    }
+                }
+                if (flag == 0)
+                {
+                    throw new InvalidOperation("不存在该订单！");
+                }
+            }catch(InvalidOperation e)
+            {
+                Console.WriteLine("商品添加异常：" + e.message);
+            }
+        }
+        public void DeleteOrderItem(int id , string item,int quantity,double price)
+        {
+            try
+            {
+                int flag = 0;
+
+                for (int i = orders.Count() - 1; i >= 0; i--)
+                {
+                    if (id == orders[i].OrderID)
+                    {
+                        flag = 1;
+                        orders[i].DeleteItem(item, quantity, price);
+                    }
+                }
+                if (flag == 0)
+                {
+                    throw new InvalidOperation("不存在该订单！");
+                }
+            }
+            catch (InvalidOperation e)
+            {
+                Console.WriteLine("商品删除异常：" + e.message);
+            }
+        }
+        public void OrderSort()
+        {
+            orders.Sort();
+        }
+        public void SearchOrders(Func func)
+        {
+            var query = from order in orders
+                        where func(order)
+                        select order;
+            foreach(Order i in query) { Console.WriteLine(i.ToString()); }
+        }
+
+    }
+}
---
+++ CSharpHomework/Homework5/OrderWarehouse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderWarehouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService orderService = new OrderService();
+            string[] customer = { "Lisa", "Tom", "Ivery" };
+            string[] goods = { "pencil", "box", "ruler" };
+            orderService.CreateOrder(0,customer[0]);
+            orderService.AddOrderItem(1, goods[0], 2, 2);
+            orderService.AddOrderItem(0, goods[0], 2, 2);
+            orderService.AddOrderItem(0, goods[1], 3, 4);
+            orderService.CreateOrder(1, customer[1]);
+            orderService.CreateOrder(2, customer[2]);
+            orderService.CreateOrder(4, customer[0]);
+            orderService.AddOrderItem(2, goods[2], 5, 10);
+            orderService.OrderSort();
+            Console.WriteLine("查找Lisa的订单");
+            orderService.SearchOrders(x => x.OwnerName == customer[0]);
+        }
+    }
+}
