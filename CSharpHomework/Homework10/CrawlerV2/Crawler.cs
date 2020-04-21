763c048dc38dea32ba5055eebe98864b34fb811a
czhhhhh <2933880537@qq.com>
2020-04-21 20:28:44 +0800

    并行执行爬虫，利用task，但是数组越界仍然没有很好解决，只是简单利用扩大数组暂时规

---
+++ CSharpHomework/Homework10/CrawlerV2/Crawler.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrawlerForm {
+  class Crawler {
+    public event Action<Crawler> CrawlerStopped;
+    public event Action<Crawler, string, string> PageDownloaded;
+
+    //所有已下载和待下载URL，key是URL，value表示是否下载成功
+    private Dictionary<string, bool> urls = new Dictionary<string, bool>();
+
+    //待下载队列
+    private ConcurrentQueue<string> pending = new ConcurrentQueue<string>();
+
+    //URL检测表达式，用于在HTML文本中查找URL
+    private readonly string urlDetectRegex = @"(href|HREF)[]*=[]*[""'](?<url>[^""'#>]+)[""']";
+
+    //URL解析表达式
+    public static readonly string urlParseRegex = @"^(?<site>https?://(?<host>[\w.-]+)(:\d+)?($|/))(\w+/)*(?<file>[^#?]*)";
+    public string HostFilter { get; set; } //主机过滤规则
+    public string FileFilter { get; set; } //文件过滤规则
+    public int MaxPage { get; set; } //最大下载数量
+    public string StartURL { get; set; } //起始网址
+    public Encoding HtmlEncoding { get; set; } //网页编码
+    public Dictionary<string, bool> DownloadedPages { get => urls; } //已下载网页
+
+    public Crawler() {
+      MaxPage = 100;
+      HtmlEncoding = Encoding.UTF8;
+    }
+
+        public void SimpleStart()
+        {
+            urls.Clear();
+            ConcurrentQueue<string> newpending = new ConcurrentQueue<string>();
+            pending = newpending;
+            pending.Enqueue(StartURL);
+            
+            while (urls.Count < MaxPage && pending.Count > 0 )
+            {
+
+                int all = pending.Count;
+                string url;
+                if (!pending.TryDequeue(out url)) continue;
+                
+                if (url != null)
+                {
+                    DownLoad(url);
+                }
+                
+            } 
+            CrawlerStopped(this);
+        }
+
+    public void Start() {
+      urls.Clear();
+      ConcurrentQueue<string> newpending = new ConcurrentQueue<string>();
+      pending = newpending;      
+      //pending.Enqueue(StartURL);
+            Task[] tasks = new Task[10000];
+            bool[] flags = new bool[10000];
+            for (int i = 0; i < 10000; i++) flags[i] = false;
+            int tail = 1;
+            DownLoad(StartURL);
+            do
+            {
+
+                int all = pending.Count;
+                string url;
+                if (!pending.TryDequeue(out url)) continue;
+                int num;
+                if (flags[urls.Count - 1] == false)
+                {
+                    num = urls.Count - 1;
+                    tail = urls.Count;
+                }
+                else
+                {
+                    num = tail;
+                    tail++;
+                }
+                flags[num] = true;
+
+
+                tasks[num] = new Task(() => DownLoad(url));
+                if (url != null)
+                {
+                    tasks[num].Start();
+                }
+
+
+            } while (urls.Count < MaxPage && pending.Count > 0 && (tasks[tail - 1].IsCompleted | !tasks[tail - 1].IsCompleted));
+      CrawlerStopped(this);
+    }
+
+    private void DownLoad(string url) {
+            try {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = urls.Count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                urls[url] = true;
+                PageDownloaded(this, url, "success");
+                Parse(html, url);
+            }catch(Exception E)
+            {
+                PageDownloaded(this, url, "  Error:" + E.Message);
+            }
+        }
+
+    private void Parse(string html, string pageUrl) {
+      var matches = new Regex(urlDetectRegex).Matches(html);
+      foreach (Match match in matches) {
+        string linkUrl = match.Groups["url"].Value;
+        if (linkUrl == null || linkUrl == "") continue;
+        linkUrl = FixUrl(linkUrl, pageUrl);//转绝对路径
+        //解析出host和file两个部分，进行过滤
+        Match linkUrlMatch = Regex.Match(linkUrl, urlParseRegex);
+        string host = linkUrlMatch.Groups["host"].Value;
+        string file = linkUrlMatch.Groups["file"].Value;
+        if (file == "") file = "index.html";
+        if (Regex.IsMatch(host, HostFilter) && Regex.IsMatch(file, FileFilter)) {
+          if (!urls.ContainsKey(linkUrl)) {
+            pending.Enqueue(linkUrl);
+            urls.Add(linkUrl, false);
+          }
+        }
+
+
+      }
+    }
+
+
+    //将相对路径转为绝对路径
+    static private string FixUrl(string url, string pageUrl) {
+      if (url.Contains("://")) {
+        return url;
+      }
+      if (url.StartsWith("//")) {
+        return "http:" + url;
+      }
+      if (url.StartsWith("/")) {
+        Match urlMatch = Regex.Match(pageUrl, urlParseRegex);
+        String site = urlMatch.Groups["site"].Value;
+        return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+      }
+
+      if (url.StartsWith("../")) {
+        url = url.Substring(3);
+        int idx = pageUrl.LastIndexOf('/');
+        return FixUrl(url, pageUrl.Substring(0, idx));
+      }
+
+      if (url.StartsWith("./")) {
+        return FixUrl(url.Substring(2), pageUrl);
+      }
+
+      int end = pageUrl.LastIndexOf("/");
+      return pageUrl.Substring(0, end) + "/" + url;
+    }
+  }
+
+   
+}
---
+++ CSharpHomework/Homework10/CrawlerV2/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace CrawlerForm {
+  partial class Form1 {
+    /// <summary>
+    /// 必需的设计器变量。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 清理所有正在使用的资源。
+    /// </summary>
+    /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows 窗体设计器生成的代码
+
+    /// <summary>
+    /// 设计器支持所需的方法 - 不要修改
+    /// 使用代码编辑器修改此方法的内容。
+    /// </summary>
+    private void InitializeComponent() {
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timelable = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.index,
+            this.URL,
+            this.status});
+            this.dgvResult.Location = new System.Drawing.Point(33, 119);
+            this.dgvResult.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.RowTemplate.Height = 23;
+            this.dgvResult.Size = new System.Drawing.Size(1353, 568);
+            this.dgvResult.TabIndex = 3;
+            // 
+            // index
+            // 
+            this.index.DataPropertyName = "Index";
+            this.index.HeaderText = "序号";
+            this.index.Name = "index";
+            // 
+            // URL
+            // 
+            this.URL.DataPropertyName = "URL";
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.Width = 500;
+            // 
+            // status
+            // 
+            this.status.DataPropertyName = "Status";
+            this.status.HeaderText = "状态";
+            this.status.Name = "status";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 693);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1417, 25);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(167, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txtUrl);
+            this.flowLayoutPanel1.Controls.Add(this.btnStart);
+            this.flowLayoutPanel1.Controls.Add(this.timelable);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(27, 25, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1417, 76);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(31, 29);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(548, 25);
+            this.txtUrl.TabIndex = 4;
+            this.txtUrl.Text = "http://www.cnblogs.com/dstang2000/";
+            this.txtUrl.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(587, 29);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 29);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "并行查询";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timelable
+            // 
+            this.timelable.AutoSize = true;
+            this.timelable.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timelable.Location = new System.Drawing.Point(741, 33);
+            this.timelable.Margin = new System.Windows.Forms.Padding(50, 8, 3, 0);
+            this.timelable.Name = "timelable";
+            this.timelable.Size = new System.Drawing.Size(59, 20);
+            this.timelable.TabIndex = 5;
+            this.timelable.Text = "Time:";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(31, 85);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 15);
+            this.lblInfo.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(903, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(100, 3, 3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "普通查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(1073, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(50, 10, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Time:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 718);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dgvResult);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+    private System.Windows.Forms.DataGridView dgvResult;
+    private System.Windows.Forms.DataGridViewTextBoxColumn index;
+    private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+    private System.Windows.Forms.DataGridViewTextBoxColumn status;
+    private System.Windows.Forms.StatusStrip statusStrip1;
+    private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    private System.Windows.Forms.TextBox txtUrl;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label timelable;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
